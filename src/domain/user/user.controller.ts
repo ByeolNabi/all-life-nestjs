59de0153cd1e28f0ee7a2150726bbe2ece8c2837
ByeolNabi <20183338@office.deu.ac.kr>
2024-09-27 12:18:16 +0900

    스웨거 설명 추가

--- src/domain/user/user.controller.ts
+++ src/domain/user/user.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { SignInDto } from './dto/signin.dto';
 import { SignUpDto } from './dto/signup.dto';
-import { ApiBody, ApiParam, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 
 @Controller('user')
 @ApiTags('유저 API')
@@ -12,6 +12,19 @@ export class UserController {
   private logger = new Logger('UserController');
 
   @Post('/signup')
+  @ApiOperation({
+    summary: '회원가입',
+    description: `
+    {
+    "user_id": "daekyu",
+    "password": "daekyu_pw",
+    "username": "김대규",
+    "email": "daekyu@email.com",
+    "term": "true",
+    "type": 0, ( 관리자 = 0, 개인사용자 = 100 )
+    "language": "한국어"
+}`,
+  })
   @ApiBody({ type: SignUpDto })
   signUp(@Body() signUpDto: SignUpDto) {
     this.logger.verbose(`UserController /signup is invoked`);
@@ -23,3 +36,15 @@ export class UserController {
     return this.userService.signIn(signInDto);
   }
 }
+
+/*
+{
+    "user_id": "daekyu",
+    "password": "daekyu_pw",
+    "username": "김대규",
+    "email": "daekyu@email.com",
+    "term": "true",
+    "type": 0,
+    "language": "한국어"
+}
+*/
